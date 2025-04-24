36d5822aaab5c4868606f4976e87606ced500eb4
gabriel silva <gabrielsilvadearaujo45@gmail.com>
2025-04-24 11:04:59 -0300

    agregando validacion en el boton de delete

--- src/calendar/components/FabDelete.jsx
+++ src/calendar/components/FabDelete.jsx
@@ -1,15 +1,16 @@
-import { useCalendarStore } from "../../hooks";
+import { useCalendarStore, useUiStore } from "../../hooks";
 
 export const FabDelete = () => {
 
     const { DeletingEvent, activeEvent } = useCalendarStore();
+    const { isDateModalOpen } = useUiStore();
 
     const handleDelete = () => {
         DeletingEvent();
     }
 
     return(
-        <button className="btn btn-danger fab-delete" onClick={handleDelete} style={{ display: activeEvent ? '' : 'none' }}>
+        <button className="btn btn-danger fab-delete" onClick={handleDelete} style={{ display: (activeEvent && !isDateModalOpen) ? '' : 'none' }}>
             <i className="fas fa-trash-alt"></i>
         </button>
     )
