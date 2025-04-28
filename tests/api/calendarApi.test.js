6ad7e2e480be49b0e8e2ac3c275d7cc3631e81f8
gabriel silva <gabrielsilvadearaujo45@gmail.com>
2025-04-28 11:18:20 -0300

    avances com las pruebas

--- tests/api/calendarApi.test.js
+++ tests/api/calendarApi.test.js
@@ -6,4 +6,14 @@ describe('API Calendar', () => {
         expect(calendarApi.defaults.baseURL).toBe(process.env.VITE_API_URL);
     });
 
+
+    // As variaveis de ambiente tem que ter o protocolo http:// porque jest nao reconhece o localhost como valido
+    test('should have token when user is logged in', async () => {
+    
+        const token = 'ABC123';
+        localStorage.setItem('token', token);
+        const res = await calendarApi.get('/auth');
+
+        expect(res.config.headers['x-token']).toBe(token);
+    });
 });
---
+++ tests/store/ui/uiSlice.test.js
@@ -0,0 +1,19 @@
+import { uiSlice, onOpenDateModal, onCloseDateModal } from '../../src/store/ui/uiSlice';
+
+describe('uiSlice', () => {
+    test('should return the initial state', () => {
+        expect(uiSlice.getInitialState()).toEqual({
+            isDateModalOpen: false
+        });
+    });
+
+    test('should toggle the date modal', () => {
+        let state = uiSlice.getInitialState();
+        state = uiSlice.reducer(state, onOpenDateModal());
+        expect(state.isDateModalOpen).toBe(true);
+
+        state = uiSlice.reducer(state, onCloseDateModal());
+        expect(state.isDateModalOpen).toBe(false);
+    });
+    
+});
