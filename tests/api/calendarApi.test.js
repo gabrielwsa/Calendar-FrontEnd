801b492375fb700ae6da6119825d748b060bdb5a
gabriel silva <gabrielsilvadearaujo45@gmail.com>
2025-04-28 11:00:24 -0300

    fix para las pruebas

--- tests/api/calendarApi.test.js
+++ tests/api/calendarApi.test.js
@@ -1,9 +1,9 @@
-import calendarApi from './calendarApi';
+import calendarApi from '../../src/api/calendarApi';
 
 describe('API Calendar', () => {
 
     test('should be default configured', () => {
-        expect(calendarApi).toBeDefined();
+        expect(calendarApi.defaults.baseURL).toBe(process.env.VITE_API_URL);
     });
 
 });
